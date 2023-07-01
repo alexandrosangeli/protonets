8ed75ba2a08c16928de53b7765faca25ade545f3
Alexandros Angeli <alexandrosangeli@gmail.com>
2023-07-01 18:21:25 +0300

    Removed numpy dependency and added support for <primitive> <op> Value

--- src/mlp.py
+++ src/mlp.py
@@ -1,97 +1,15 @@
-import numpy as np
-
-
-class Value:
-
-    def __init__(self, data, children=[], label=''):
-        self.data = data
-        self.grad = 0.0
-        self.label = label
-        self.prev = children
-        self._backward = lambda : None
-
-    def __repr__(self):
-        return f"Value(data={self.data}, grad={self.grad})"
-
-    def __add__(self, other):
-        other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data + other.data, [self, other], f"({self.label})+{other.label}")
-
-        def _backward():
-            self.grad += 1.0 * out.grad
-            other.grad += 1.0 * out.grad
-            
-        out._backward = _backward
-        return out
-    
-    def __sub__(self, other):
-        out = Value(self.data - other.data, [self, other], f"({self.label})+{other.label}")
-
-        def _backward():
-            self.grad += 1.0 * out.grad
-            other.grad += 1.0 * out.grad
-            
-        out._backward = _backward
-        return out
-
-    def __mul__(self, other):
-        other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data * other.data, [self, other], f"({self.label})*{other.label}")
-
-        def _backward():
-            self.grad += other.data * out.grad
-            other.grad += self.data * out.grad
-            
-        out._backward = _backward
-        return out
-    
-    def __pow__(self, other):
-        out = Value(self.data ** other.data, [self, other], f"({self.label})^{other.label}")
-        
-        def _backward():
-            self.grad += other.data * self.data * out.grad
-            sign = 1 if self.data > 0 else -1
-            other.grad += self.data ** other.data * sign * np.log(abs(self.data)) * out.grad
-            
-        out._backward = _backward
-        return out
-    
-    def sig(self):
-        y = np.exp(self.data) / (np.exp(self.data) + 1)
-        out = Value(y, [self], f"(sig({self.label})")
-        
-        def _backward():
-            self.grad += y * (1 - y)
-            
-        out._backward = _backward
-        return out
-    
-
-    def backward(self):
-        topo = []
-        visited = set()
-        def topo_sort(node):
-            if node not in visited:
-                visited.add(node)
-                for child in node.prev:
-                    topo_sort(child)
-                topo.append(node)
-
-        self.grad = 1
-        topo_sort(self)
-        nodes = reversed(topo)
-        for node in nodes:
-            node._backward()
+from backbone import Value
+from utils import rand_normal, dot
 
 
 class Neuron:
 
     def __init__(self, num_input):
-        self.w = [Value(w) for w  in np.random.randn(num_input)]
-        self.b = Value(np.random.randn(1)[0])
+        self.w = [Value(w) for w  in rand_normal(num_input)]
+        self.b = Value(rand_normal(1)[0])
 
     def __call__(self, xs):
-        out = np.dot(self.w, xs) + self.b
+        out = dot(self.w, xs) + self.b
         return out.sig()
     
 
@@ -114,7 +32,7 @@ class Layer:
 class MLP:
 
     def __init__(self, num_inputs, outs):
-        # number of outputs is for each layers
+        # number of outputs is for each layer
         self.layers = [Layer(num_inputs, outs[0])] + [Layer(outs[i], outs[i+1]) for i in range(len(outs) - 1)]
 
     def __call__(self, xs):
---
+++ src/utils.py
@@ -0,0 +1,50 @@
+import random
+import math
+
+
+def rand_normal(n):
+    """
+        Return n samples from the standard normal distribution.
+
+        - Uses the Box-Muller transform method: https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
+        - This is a fairly efficient method. See rand_normal_slow for a (slightly) simpler implementation
+        - Takes approx. half the time to execute when drawing 10000 samples (from an avg. of 1000 executions)
+    """
+    if n < 1:
+        return None
+
+    rands = []
+    i = 0
+    for i in range(n//2 + 1):
+        u1 = random.uniform(0, 1)
+        u2 = random.uniform(0, 1)
+
+        z0 = math.sqrt(-2 * math.log(u1)) * math.cos(2 * math.pi * u2)
+        z1 = math.sqrt(-2 * math.log(u1)) * math.sin(2 * math.pi * u2)
+        
+        rands.append(z0)
+        rands.append(z1)
+
+    return rands[:n]
+
+
+def rand_normal_slow(n):
+    if n < 1:
+        return None
+    
+    rands = []
+    for i in range(n):
+        u1 = random.uniform(0, 1)
+        u2 = random.uniform(0, 1)
+        
+        z0 = math.sqrt(-2 * math.log(u1)) * math.cos(2 * math.pi * u2)
+        z1 = math.sqrt(-2 * math.log(u1)) * math.sin(2 * math.pi * u2)
+
+        rands.append(z0)
+
+    return rands
+
+
+def dot(arr0, arr1):
+    # Calculates the dot product between two arrays
+    return sum(x*y for x, y in zip(arr0, arr1))
